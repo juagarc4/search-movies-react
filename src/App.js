f6e42f7a999189e874269f5fcab617267cd8bcdb
Raul Garcia <garcia@publicplan.de>
2020-09-05 15:26:54 +0200

    Added component ResultsList

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import 'bulma/css/bulma.css';
 import {Title} from "./components/Title";
 import {SearchForm} from "./components/SearchForm";
 
-function App() {
-  return (
-    <div className="App">
-        <Title>Search movies</Title>
-        <div className='SearchForm-wrapper'>
-            <SearchForm />
-        </div>
-    </div>
-  );
+class App extends Component {
+    state = { results: [] }
+
+    _handleResults = (results) => {
+        this.setState({ results })
+    }
+
+    _renderResults = () => {
+        const { results } = this.state
+        return results.map(movie => {
+            return <p key={movie.imdbID}>{movie.Title}</p>
+        })
+    }
+    render() {
+        return (
+            <div className="App">
+                <Title>Search movies</Title>
+                <div className='SearchForm-wrapper'>
+                    <SearchForm onResults={this._handleResults} />
+                </div>
+                {this.state.results.length === 0
+                    ? <p>No results found</p>
+                    : this._renderResults()
+                }
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,17 +1,24 @@
 import React, {Component} from 'react';
 
+const API_KEY = '4287ad07'
+
 export class SearchForm extends Component {
     state = {
         inpuMovie: '',
-
     }
     _handleChange = (e) => {
         this.setState({inputMovie: e.target.value})
     }
     _handleSubmit = (e) => {
         e.preventDefault()
-        alert(this.state.inputMovie)
-        // this.setState({ inputMovie: e.target.value })
+        const { inputMovie } = this.state
+        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${inputMovie}`)
+            .then(res => res.json())
+            .then(results => {
+                const { Search, totalResults } = results
+                console.log(Search, totalResults)
+                this.props.onResults(Search)
+            })
     }
 
     render() {
