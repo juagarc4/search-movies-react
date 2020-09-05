974f0c64f2e8df964c32751f5df6484518066385
Raul Garcia <garcia@publicplan.de>
2020-09-05 14:50:40 +0200

    Added component SearchForm

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './App.css';
 import 'bulma/css/bulma.css';
 import {Title} from "./components/Title";
+import {SearchForm} from "./components/SearchForm";
 
 function App() {
   return (
     <div className="App">
         <Title>Search movies</Title>
-
+        <div className='SearchForm-wrapper'>
+            <SearchForm />
+        </div>
     </div>
   );
 }
