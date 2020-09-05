0e111e1d0484955e6335a11a3f09e9d20486c2e7
Raul Garcia <garcia@publicplan.de>
2020-09-05 20:17:25 +0200

    Basic-routing: Showing a details page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import 'bulma/css/bulma.css';
 import {Title} from "./components/Title";
 import {SearchForm} from "./components/SearchForm";
 import {MoviesList} from "./components/MoviesList";
+import {Detail} from "./pages/Detail";
 
 class App extends Component {
     state = { usedSearch: false, results: [] }
@@ -17,6 +18,12 @@ class App extends Component {
             : <MoviesList movies={this.state.results} />
     }
     render() {
+        const url = new URL(document.location)
+        const hasId = url.searchParams.has('id')
+
+        if (hasId) {
+            return <Detail id={url.searchParams.get('id')} />
+        }
         return (
             <div className="App">
                 <Title>Search movies</Title>
